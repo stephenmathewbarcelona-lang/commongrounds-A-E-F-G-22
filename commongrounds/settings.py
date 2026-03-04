aa07c264e63f2b853f5b3516f245ac282e32ec84
JohnCyren <john.cyren.roxas@student.ateneo.edu>
2026-03-04 21:48:09 +0800

    diyprojects app addition

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'diyprojects',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'commongrounds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
