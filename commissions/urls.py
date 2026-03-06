d37dbc8cfa75131fb1e61532457f7f65bfb0c9f4
stephenmathewbarcelona <stephen.mathew.barcelona@student.ateneo.edu>
2026-03-06 22:30:40 +0800

    Revert "Commissions"

--- commissions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CommissionRequestsListView, CommissionRequestsDetailView
-
-urlpatterns = [
-    path('commissions/requests', CommissionRequestsListView.as_view(), name="commissions-list"),
-    path('commissions/request/<int:pk>', CommissionRequestsDetailView.as_view(), name='commissions-detail'),
-]
-
-app_name = 'commissions'
