8191e6ef33a3a613a251d19776d301ee582a2223
stephenmathewbarcelona <stephen.mathew.barcelona@student.ateneo.edu>
2026-03-06 22:29:48 +0800

    Revert "Diyprojects"

--- diyprojects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('projects/', views.ProjectList_View.as_view(), name='project_list'),
-    path('project/<int:pk>/', views.ProjectDetail_View.as_view(), name='project_detail'),
-]
