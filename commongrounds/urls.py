471dacaecb96a1d59f4b46c37375969a00edd773
OwlofWis <yap.ethan040406@gmail.com>
2026-03-06 22:24:34 +0800

    Update urls.py

--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView # Add this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('localevents/', include('localevents.urls')),
-    
-    path('', RedirectView.as_view(url='/localevents/events/', permanent=True)),
+    path('', include('localevents.urls')), 
 ]
