f0bfb3cd7bcfa311692b67dd6c41525d441d0457
OwlofWis <yap.ethan040406@gmail.com>
2026-05-07 09:36:40 +0800

    Update urls.py

--- localevents/urls.py
+++ localevents/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'localevents'
 
 urlpatterns = [
-    path('', views.EventListView.as_view(), name='event_list'),
+    path('events/', views.EventListView.as_view(), name='event_list'),
     path('event/<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('event/add/', views.EventCreateView.as_view(), name='event_create'),
+    path('event/<int:pk>/edit/', views.EventUpdateView.as_view(), name='event_update'),
+    path('event/<int:pk>/signup/', views.EventSignupView.as_view(), name='event_signup'),
 ]
-
