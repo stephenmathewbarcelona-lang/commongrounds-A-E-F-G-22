47ff7341e83c2f75698fc464d566431403ca4946
stephenmathewbarcelona-lang <stephen.mathew.barcelona@student.ateneo.edu>
2026-05-07 22:04:03 +0800

    Update settings.py

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['commongrounds-22-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if os.getenv('CSRF_TRUSTED_ORIGINS') else []
 
 
 # Application definition
