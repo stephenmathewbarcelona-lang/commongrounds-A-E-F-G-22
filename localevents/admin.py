d36839792d60eb217748703aadd586409459068f
OwlofWis <yap.ethan040406@gmail.com>
2026-05-07 09:39:18 +0800

    Update admin.py

--- localevents/admin.py
+++ localevents/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import EventType, Event
+from .models import Event, EventType, Profile, EventSignup
 
-admin.site.register(EventType)
+admin.site.register(Profile)
 admin.site.register(Event)
+admin.site.register(EventType)
+admin.site.register(EventSignup)
